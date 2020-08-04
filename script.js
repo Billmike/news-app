5d01a2c3727e143d75853e9c73c61a74a1e5c7cd
Billmike <ayelegun.kayode@gmail.com>
2020-08-04 13:20:45 +0100

    Display image properly

--- script.js
+++ script.js
@@ -2,24 +2,22 @@ const searchNews = document.getElementById('search-news');
 const showNews = document.getElementById('show-news');
 const submit = document.getElementById('submit-btn');
 
-const getNews = async() => {
-  
-    const res = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=OtN5TgEakuRqnuUsHHxWTfMISpfrrAV7
-    `)
-    let data = await res.json();
-    console.log(data)
+const getNews = async () => {
+  const res = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=OtN5TgEakuRqnuUsHHxWTfMISpfrrAV7
+    `);
+  let data = await res.json();
+  console.log(data);
 
-    const details = data.results;
-    const showDetails = details.map(
-        item => {
-          return `
+  const details = data.results;
+  const showDetails = details.map((item) => {
+    return `
           <div class="container-fluid">
           <div class="row">
               <div class="col-12 mt-3">
                   <div class="card">
                       <div class="card-horizontal">
                           <div class="img-square-wrapper">
-                              <img class="" src="http://via.placeholder.com/300x180" alt="Card image cap">
+                              <img class="image" src=${item.multimedia[0].url} alt="Card image cap">
                           </div>
                           <div class="card-body">
                               <h4 class="card-title">${item.title}</h4>
@@ -33,11 +31,9 @@ const getNews = async() => {
               </div>
           </div>
       </div>
-          `
-        }  
-    )   
-  showNews.innerHTML = showDetails
-  
-}
+          `;
+  });
+  showNews.innerHTML = showDetails;
+};
 
-submit.addEventListener('click', getNews)
+submit.addEventListener('click', getNews);
